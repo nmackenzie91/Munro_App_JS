a8912f86c00a76d5f8d5e325bac05863496955ea
nmackenzie91 <nmackenzie@live.co.uk>
2020-05-09 20:22:12 +0100

    Initial files, basic HTML and CSS

---
+++ js/app.js
@@ -0,0 +1,25 @@
+/*plan of apption
+
+Learning Objectives
+Be able to attach event listeners to DOM elements to respond to events
+Be able to access data from the event object
+Be able to use the document object to add information to the page by manipulating the DOM
+
+Brief
+Your task is to create a front-end JavaScript application that allows users to input items and see them displayed in a list. 
+Choose a theme for the application, such as a list of endangered Animals / favourite Sports Stars / any other theme of your choice. 
+Users should be able to enter values for different properties relating to the theme (Name, Species and Continent, for example) 
+and see them displayed below.
+
+MVP
+Create a form in HTML with inputs for relevant data
+When the form is submitted, access the data from the form in the form's submit event object
+Create a list in HTML
+Append the submitted data to the list
+Add a "Delete All" <button> which clears all of the list items from the list
+
+
+
+
+
+*/
